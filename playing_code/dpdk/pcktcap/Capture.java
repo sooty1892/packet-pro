ecc1f085b803e18c3b4d4953de923861c7428360
Ashley Hemingway <ashley.hemingway12@imperial.ac.uk>
2015-02-25 05:15:08 +0000

    Nearly finished interim report - need to add more background

--- playing_code/dpdk/pcktcap/Capture.java
+++ playing_code/dpdk/pcktcap/Capture.java
@@ -2,11 +2,37 @@ public class Capture {
 
 	private class Rte_eth_conf {
 		public Rte_eth_conf() {};
-		public Rte_eth_rxmode;
+		public Rte_eth_rxmode rxmode;
+		public Rte_eth_txmode txmode;
+		Rte_eth_rss_conf rss_conf;
 	}
 
 	private class Rte_eth_rxmode {
 		public Rte_eth_rxmode() {};
+		public int split_hdr_size;
+		public int header_split;
+		public int hw_ip_checksum;
+		public int hw_vlan_filter;
+		public int jumbo_frame;
+		public int hw_strip_crc;
+		public String mq_mode;
+	}
+
+	private class Rte_eth_txmode {
+		public Rte_eth_txmode() {};
+		public String mq_mode;
+	}
+
+	private class Rte_eth_rss_conf {
+		public Rte_eth_rss_conf() {};
+		public int rss_key;
+		public int rss_hf;
+	}
+
+	private void setup() {
+		Rte_eth_rxmode rxmode = new Rte_eth_rxmode();
+		Rte_eth_txmode txmode = new Rte_eth_txmode();
+		Rte_eth_rss_conf rss_conf = new Rte_eth_rss_conf();
 	}
 
 	private static int MAX_PKT_BURST = 512;
@@ -16,19 +42,22 @@ public class Capture {
 	private static int NB_RX_DESC = 256;
 	private static int NB_TX_DESC = 256;
 
-	//structs here
-
 	public static void main(String[] args) {
+
+		setup();
+
 		int i = 0;
 		int ret = 0;
 		int recv_cnt = 0;
 		int ifidx = 0;
-		int count = 0;
 
 		//structs here
 
 		//initialise eal
 		Wrapper.wrap_eal_init(args);
+
+		int count = Wrapper.wrap_eth_dev_count();
+		System.out.println("eth ports = " + count);
 
 		//configure ethernet devices
 
--- playing_code/dpdk/pcktcap/Wrapper.java
+++ playing_code/dpdk/pcktcap/Wrapper.java
@@ -1,11 +1,16 @@
 public class Wrapper {
 	
 		private static native int eal_init(String[] args, int num);
+		private static native int eth_dev_count();
 
     	static { System.loadLibrary("Wrapper"); }
 	
 		public static int wrap_eal_init(String[] args) {
 			return eal_init(args, args.length);
+		}
+
+		public static int wrap_eth_dev_count() {
+			return eth_dev_count();
 		}
 	
 }
