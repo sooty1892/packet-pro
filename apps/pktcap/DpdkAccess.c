3257884b2301286f016bdca50c1855ed84463651
Ashley Hemingway <ashemingway.un@gmail.com>
2015-05-17 23:57:13 +0100

    Pktcap app now checks ports

--- apps/pktcap/DpdkAccess.c
+++ apps/pktcap/DpdkAccess.c
@@ -15,10 +15,16 @@
 
 JNIEXPORT jint JNICALL Java_DpdkAccess_nat_1setup_1and_1conf(JNIEnv *env, jclass class) {
 	char *strs[] = {"Pktcap", "-c", "0x01", "-n", "1"};
-	int i;
+	/*int i;
 	for (i=0; i < 5; i++) {
 		printf("C String: %s\n", strs[i]);
-	}
+	}*/
 	int ret = rte_eal_init(5, strs);
+	int count = rte_eth_dev_count();
+	if (count <= 0) {
+		rte_panic("%i Ports Enabled!!!", count);
+	} else {
+		printf("C: %d ports enabled\n", count);
+	}
 	return ret;
 }
