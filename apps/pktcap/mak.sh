a15f6b5f6cd68825d4871f9912b6debdc296e486
Ashley Hemingway <ashemingway.un@gmail.com>
2015-05-18 17:49:20 +0100

    Fixed dependcy issue when running full apps with shared library

--- apps/pktcap/mak.sh
+++ apps/pktcap/mak.sh
@@ -1,5 +1,5 @@
 make clean
 javac Pktcap.java DpdkAccess.java
 make
-sudo cp /home/dpdk64/packet-pro/apps/pktcap/build/lib/libnat_dpdk.so /usr/lib/x86_64-linux-gnu
+sudo \cp /home/dpdk64/packet-pro/apps/pktcap/build/lib/libnat_dpdk.so /usr/lib/x86_64-linux-gnu
 sudo ldconfig
