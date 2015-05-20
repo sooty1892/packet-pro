ff861733169c7dcfdcfdb130f3b196677e761ca3
Ashley Hemingway <ashley.hemingway12@imperial.ac.uk>
2015-05-20 16:27:19 +0100

    Added required include files

--- apps/pktcap/DpdkAccess.c
+++ apps/pktcap/DpdkAccess.c
@@ -1,19 +1,43 @@
 #include "DpdkAccess.h"
 
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <inttypes.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/queue.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <getopt.h>
 
+#include <rte_common.h>
+#include <rte_byteorder.h>
+#include <rte_log.h>
 #include <rte_memory.h>
+#include <rte_memcpy.h>
 #include <rte_memzone.h>
-#include <rte_launch.h>
-#include <rte_tailq.h>
 #include <rte_eal.h>
 #include <rte_per_lcore.h>
+#include <rte_launch.h>
+#include <rte_atomic.h>
+#include <rte_cycles.h>
+#include <rte_prefetch.h>
 #include <rte_lcore.h>
+#include <rte_per_lcore.h>
+#include <rte_branch_prediction.h>
+#include <rte_interrupts.h>
+#include <rte_pci.h>
+#include <rte_random.h>
 #include <rte_debug.h>
+#include <rte_ether.h>
 #include <rte_ethdev.h>
+#include <rte_ring.h>
+#include <rte_mempool.h>
 #include <rte_mbuf.h>
+#include <rte_malloc.h>
+#include <rte_fbk_hash.h>
+#include <rte_ip.h>
 
 #define ERROR -1;
 
