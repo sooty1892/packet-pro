84c3d2a0806491967509816df54b8635c4a66de7
Ashley Hemingway <ashley.hemingway12@imperial.ac.uk>
2015-05-24 21:29:33 +0100

    Java now polling the queue

--- apps/pktcap/DpdkAccess.java
+++ apps/pktcap/DpdkAccess.java
@@ -2,10 +2,8 @@ import java.nio.ByteBuffer;
 
 public class DpdkAccess {
 
-	///private static native int eal_init(int num, String[] args);
-	//private static native int lcore_id();
-	//private static native void mp_wait_lcore();
 	private static native int nat_setup_and_conf(ByteBuffer buffer);
+	private static native int nat_get_packets(ByteBuffer buffer);
 
 	static { System.loadLibrary("nat_dpdk"); }
 
@@ -13,23 +11,8 @@ public class DpdkAccess {
 		return nat_setup_and_conf(buffer);
 	}
 
-	//initialise eal
-	/*public static int eal_init_wrap(String[] args) {
-		for (String st : args) {
-			System.out.println("JAVA: arg " + st);
-		}
-		return eal_init(args.length, args);
+	public static int get_packets(ByteBuffer buffer) {
+		return nat_get_packets(buffer);
 	}
-
-	//get id of current core
-	public static int lcore_id_wrap() {
-		return lcore_id();
-	}
-
-	//wait until all cores have finished
-	public static void mp_wait_lcore_wrap() {
-		mp_wait_lcore();
-	}*/
-
 
 }
--- apps/pktcap/Pktcap.java
+++ apps/pktcap/Pktcap.java
@@ -7,8 +7,18 @@ public class Pktcap implements Runnable {
 	public static void main(String[] args) {
         System.out.println("JAVA: Starting Java app");
         buffer = ByteBuffer.allocateDirect(8*1024);
-        (new Thread(new Pktcap())).start();
-        (new Thread(new PacketInspector(buffer))).start();
+        //(new Thread(new Pktcap())).start();
+        //(new Thread(new PacketInspector(buffer))).start();
+        if (DpdkAccess.setup_and_conf() < 0) {
+        	System.out.println("JAVA: Error in DPDK setup ");
+			return;
+        }
+        System.out.println("JAVA: Starting recieve queue polling");
+        while (true) {
+        	buffer = ByteBuffer.allocateDirect(8*1024);
+        	int count = DpdkAccess.get_packets(buffer);
+        	System.out.println("JAVA: Received " + count + " packets");
+        }
 	}
 	
 	public void run() {
