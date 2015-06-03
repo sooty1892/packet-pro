5ed01677ff4846035bcf55e2ccb33bb71a27c14e
Ashley Hemingway <ashemingway.un@gmail.com>
2015-06-03 15:10:02 +0100

    Fixed script to ignore unsafe warnings

--- apps/firewall/mak.sh
+++ apps/firewall/mak.sh
@@ -1,6 +1,6 @@
 make clean
 rm -f hs_err*
-javac *.java
+javac -XDignore.symbol.file *.java
 make
 sudo \cp /home/dpdk64/packet-pro/apps/firewall/build/lib/libnat_dpdk.so /usr/lib/x86_64-linux-gnu
 sudo ldconfig
