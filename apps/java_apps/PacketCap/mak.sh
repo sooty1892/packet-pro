bb1a26d95385b84c98ab935e8af1904260458a50
Ashley Hemingway <ashley.hemingway12@imperial.ac.uk>
2015-06-13 21:29:13 +0100

    fixed path location

--- apps/java_apps/PacketCap/mak.sh
+++ apps/java_apps/PacketCap/mak.sh
@@ -5,7 +5,7 @@ find ../DpdkJava/ -name "*.java" >> sources.txt
 javac -XDignore.symbol.file @sources.txt
 rm -f sources.txt
 make
-sudo \cp ~/packet-pro/apps/PacketCap/build/lib/libnat_dpdk.so /usr/lib/x86_64-linux-gnu
+sudo \cp ~/packet-pro/apps/java_apps/PacketCap/build/lib/libnat_dpdk.so /usr/lib/x86_64-linux-gnu
 sudo ldconfig
 
 find ./src/  -name "*.class" > classes.txt
